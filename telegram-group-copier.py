b33cf5270b25a050e7c6082bc92ee242640551c0
Solid Marketing <189283094+solidmarkt@users.noreply.github.com>
2025-04-16 08:49:30 +0300

    Add files via upload

---
+++ telegram-group-copier.py
@@ -0,0 +1,16 @@
+#Telegram group copier/group mirroring
+
+#This file has no features, please review the README.MD file.
+
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
